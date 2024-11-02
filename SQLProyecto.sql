13911544f2b1c6dc92828dd3c6e4ba305fce79ed
Satru <130175521+ImSatru@users.noreply.github.com>
2024-11-02 14:13:09 -0500

    add: SQL file

---
+++ SQLProyecto.sql
@@ -0,0 +1,18 @@
+use backend;
+create table usuario(
+id int not null primary key auto_increment,
+documento varchar (100),
+nombre  varchar (150),
+correo varchar (150),
+contraseña varchar(30)
+);
+
+create table evento (
+id int not null primary key auto_increment,
+nombre varchar (100),
+lugar  varchar (150),
+descripcion varchar (150),
+fecha date,
+disponibilidad int,
+hora datetime
+)
